99bd5040730550919cb2c51445ba126459ea7735
linuxlonelyeagle <2020382038@qq.com>
2021-07-23 20:16:34 +0800

    更新版本

--- 测试/my_chatcli.c
+++ 测试/my_chatcli.c
@@ -15,11 +15,13 @@
 #include<sys/epoll.h>
 #include<semaphore.h>
 #include<fcntl.h>
-
+#include<sys/stat.h>
+#include<sys/sendfile.h>
 #define MAXEVENTS 64
 
 
-
+//用来保存文件
+FILE *fp = NULL;
 pthread_t pth;
 
 pthread_mutex_t sign_in_mutex;
@@ -48,6 +50,7 @@ int have_sign_in = 0;
 int efd;
 struct epoll_event event;
 struct epoll_event *events = NULL;
+
 
 //设置文件描述符非阻塞
 int 
@@ -172,6 +175,9 @@ scan_group_chat_message(int connfd);
 void 
 deliver_file(int connfd);
 
+void
+receive_file(int connfd);
+
 int
 main(int argc,char *argv[])
 {
@@ -180,6 +186,7 @@ main(int argc,char *argv[])
     signal(SIGPIPE,SIG_IGN);
     
     char op;
+    char cmd[1024];
     int port = atoi(argv[2]);
     int connfd = open_clientfd(argv[1],port);
     //创建一个专门用来接收消息的线程
@@ -201,9 +208,17 @@ main(int argc,char *argv[])
         start();
         printf("my_chat:");
         fflush(stdout);
-        scanf("%c",&op);
+        scanf("%s",cmd);
         while(getchar()!='\n')
             ;
+        if(strlen(cmd) != 1)
+        {
+            printf("输入错误,请重新输入。\n");
+            break;
+        }
+        else if(strlen(cmd) == 1)
+        op = cmd[0];
+        memset(cmd,0,1024);
         switch(op)
         {
             case 'a':
@@ -233,19 +248,30 @@ main(int argc,char *argv[])
                  printf("输入错误，请重新输入.\n");
                  break;
         }
+        }
         while(have_sign_in ==1 )
         {
             have_in();
             fflush(stdout);
             printf("my_chat:");
             fflush(stdout);
-            scanf("%c",&op);
+            scanf("%s",cmd);
             while(getchar()!='\n')
                 ;
+            if(strlen(cmd) != 1)
+            {
+                printf("输入错误，请重新输入.\n");
+                break;
+            }
+            else if(strlen(cmd) == 1)
+            op = cmd[0];
+            memset(cmd,0,1024);
+            printf("%c\n",op);
+            
             switch(op)
             {
                 case 'a':
-                    private_chat(connfd);
+            private_chat(connfd);
             pthread_mutex_lock(&my_chat_mutex);
             pthread_cond_wait(&my_chat_cond,&my_chat_mutex);
             pthread_mutex_unlock(&my_chat_mutex);
@@ -367,9 +393,9 @@ main(int argc,char *argv[])
                     break;
                 case 'v':   //传输文件
                     deliver_file(connfd);
-                    pthread_mutex_lock(&my_chat_mutex);
-                    pthread_cond_wait(&my_chat_cond,&my_chat_mutex);
-                    pthread_mutex_unlock(&my_chat_mutex);
+                    break;
+                case 'y' :  //接受文件
+                    receive_file(connfd);
                     break;
                 default:
                     printf("输入错误,请重新输入.\n");
@@ -380,7 +406,6 @@ main(int argc,char *argv[])
         }
         
     }
-    }
 }
 
 void 
@@ -388,6 +413,7 @@ deliver_file(int connfd)
 {
     char path[100];
     char buf[1024];
+    char read_buf[512];
     char recv_account[30];
     printf("请输入你要发送的用户的帐号:");
     fflush(stdout);
@@ -395,24 +421,24 @@ deliver_file(int connfd)
     while(getchar()!='\n')
         ;
     printf("请输入你传入的文件名字:");
-    fflush(stdout);
     scanf("%s",path);
     while(getchar()!='\n')
         ;
-    sprintf(buf,"{\"deliver_file\":{\"path\":\"%s\",\"recv_account\":\"%s\"}}",path,recv_account);
-    send(connfd,buf,1024,0);
-    printf("开始传输文件、\n");
-    FILE * fp = NULL;
-    fp = fopen(path,"r");
-    while(fread(buf,1,1024,fp)>0)
+
+    int fd = open(path,O_RDONLY);
+    if(fd < 0)
     {
-       send(connfd,buf,1024,0) ;
+        printf("打开文件失败\n");
+        return ;
     }
-    fread(buf,1,1024,fp);
+    struct stat stat_buf;
+    fstat(fd,&stat_buf);
+    sprintf(buf,"{\"deliver_file\":{\"path\":\"%s\",\"recv_account\":\"%s\",\"deliver_account\":\"%s\"}}",path,recv_account,ps.account);
     send(connfd,buf,1024,0);
-    printf("%s\n",buf);
-    strcpy(buf,"NULL");
+    sendfile(connfd,fd,NULL,stat_buf.st_size);
+    sprintf(buf,"null");
     send(connfd,buf,1024,0);
+    printf("传输文件成功");
 }
 
 void 
@@ -631,7 +657,13 @@ void
 scan_msg(int connfd)
 {
     char buf[1024];
-    sprintf(buf,"{\"scan_msg\":{\"account\":\"%s\"}}",ps.account);
+    memset(buf,0,1024);
+    char fri_account[30];
+    printf("输入你要查看的记录的好友的帐号:");
+    scanf("%s",fri_account);
+    while(getchar()!='\n')
+        ;
+    sprintf(buf,"{\"scan_msg\":{\"account\":\"%s\",\"fri_account\":\"%s\"}}",ps.account,fri_account);
     send(connfd,buf,1024,0);
 }
 
@@ -713,6 +745,7 @@ pthread(void *arg)
            if(events[i].events&EPOLLIN)
            {
                 recv(fd,buf,1024,0);
+                printf("%s\n",buf);
                 find_function(fd,buf);
                 pthread_cond_signal(&my_chat_cond);
            }
@@ -720,6 +753,32 @@ pthread(void *arg)
         }
     }
 
+}
+
+void receive_file(int connfd)
+{
+    char name[50];
+    char way[30];
+    char buf[1024];
+    printf("输入你想要接受或拒绝的文件");
+    scanf("%s",name);
+    while(getchar()!='\n');
+    ;
+    while(1)
+    {
+        printf("输入yes或者no接收或者拒绝文件:");
+        scanf("%s",way);
+        while(getchar()!='\n')
+            ;
+        if(!strcmp(way,"yes"))
+            break;
+        else if(!strcmp(way,"no"))
+            break;
+        printf("输入错误，请重新输入");
+    }
+    sprintf(buf,"{\"receive_file\":{\"account\":\"%s\",\"way\":\"%s\",\"name\":\"%s\"}}",ps.account,way,name);
+    send(connfd,buf,1024,0);
+    printf("发送成功\n");
 }
 
 
@@ -755,8 +814,8 @@ find_function(int fd,char *str)
     char buf[30];
     int j = 0;
     char *find = strchr(str,':');
-    if(find == NULL)   //没有输入正确直接推出
-        return ;
+    if(find != NULL)   //没有输入正确直接推出
+   {
     for(char *i = str+2;i!=find;i++)
     {
         if(*i != '\"')
@@ -813,7 +872,7 @@ find_function(int fd,char *str)
         cJSON *item = NULL;
         root = cJSON_Parse(str);
         item = cJSON_GetObjectItem(root,"sign_up");
-        printf("%s\n",item->valuestring);
+        printf("你的帐号是:%s\n",item->valuestring);
         pthread_cond_broadcast(&sign_in_cond);
         printf("注册结束、、\n");
     }
@@ -1045,36 +1104,10 @@ find_function(int fd,char *str)
     {
         cJSON *root =NULL;
         cJSON *item =NULL;
-        cJSON *next = NULL;
-
-        char buf[1024];
-        char account[30];
-        char name[30];
-        printf("%s\n",str);
         root = cJSON_Parse(str);
         item = cJSON_GetObjectItem(root,"deliver_file");
-        next = cJSON_GetObjectItem(item,"way");
-        printf("%s\n",next->valuestring);
-        if(strcmp("begin",next->valuestring)==0)
-        {
-            next = cJSON_GetObjectItem(item,"recv_account");
-            strcpy(account,next->valuestring);
-            next = cJSON_GetObjectItem(item,"path");
-            strcpy(name,next->valuestring);
-            printf("帐号为:%s 给你发来 %s 文件\n",account,name);
-            FILE *fp=fopen(name,"w");
-            while(recv(fd,buf,1024,0))
-            {
-                if(strcmp(buf,"NULL")==0)
-                    break;
-                fwrite(buf,1,1024,fp);
-            }
-            fflush(stdout);
-        }
-        else if(strcmp("end",next->valuestring)==0)
-        {
-            printf("发送文件结束\n");
-        }
+        printf("%s\n",item->valuestring);
+
     }
         else if(strcmp("find_back",buf)==0)
         {
@@ -1122,9 +1155,35 @@ find_function(int fd,char *str)
                 pthread_cond_signal(&find_back_cond);
             find_back_flag = 0;
         }
-    
-
-
+        else if( strcmp(buf,"receive_file")==0 )
+        {
+            cJSON *root = NULL;
+            cJSON *item = NULL;
+            cJSON *next = NULL;
+            char file_name[50];
+            root = cJSON_Parse(str);
+            item = cJSON_GetObjectItem(root,"receive_file");
+            next = cJSON_GetObjectItem(item,"file_name");
+            strcpy(file_name,next->valuestring);
+            next = cJSON_GetObjectItem(item,"way");
+            char path[50];
+            sprintf(path,"%s.rec",file_name);
+            if(strcmp("begin",next->valuestring)==0)
+            {
+                printf("开始接受文件\n");
+                fp =  fopen(path,"a");
+            }
+            else if(strcmp("end",next->valuestring)==0)
+            {
+                printf("接受文件完成\n");
+            }
+        }
+    }
+    else
+    {
+        fwrite(str,1,1024,fp);
+        printf("%s",str);
+    }
 }
 
 
@@ -1199,6 +1258,7 @@ have_in()
     printf("T.踢人、\n");
     printf("U.查看聊天记录、\n");
     printf("V.传输文件、\n");
+    printf("Y.接受或者拒绝文件、\n");
     printf("X.退出帐号、\n");
     printf("*************************\n");
 }
