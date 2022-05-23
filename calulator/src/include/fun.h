724dc07091dacb618ae667c93506db9cbc919658
lonely eagle <75576166+linuxlonelyeagle@users.noreply.github.com>
2022-05-23 23:28:42 +0800

    Delete calulator directory

--- calulator/src/include/fun.h
+++
@@ -1,5 +0,0 @@
-#define is_op(ch) ch == '+' || ch =='-' || ch == '(' || ch ==')' || ch == '*' || ch == '/' || ch =='#'
-char compare(char a, char b);
-double op(double a, double b ,char c);
-void help();
-void shoutdown(void);
